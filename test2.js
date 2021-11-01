58eb974e78881340bbd88bb8687f7063e32b4d3f
雛野綾桜 <aira@hinasense.jp>
2021-11-02 02:29:00 +0900

    Revert "a"
    This reverts commit d8745b7b6a15c394f639e08a5ba5bfdda49f7e43.

--- test2.js
+++
@@ -1,8 +0,0 @@
-const syanten = require('./index')
-let hai = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [11, 0, 0, 0, 0, 0, 0]
-]
-console.log(syanten.hairi(hai))
